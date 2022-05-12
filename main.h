c9e32d50b506ef58cdc0b4a0d6c149621d5d5be6
felixokolo <pheelix.info@gmail.com>
2022-05-12 21:38:08 +0000

    addition

--- main.h
+++ main.h
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define SHELL_STR "Simple_shell"
 
 int simple_shell(void);
 
--- shell.c
+++ shell.c
@@ -6,40 +6,54 @@
  */
 int main(int argc, char **argv)
 {
-	char *av = NULL, *token, *str, *arg[20], *svptr;
+	char *av = NULL, *token, *str, *arg[1024], *svptr;
 	size_t len = 0, size;
-	int n = 0;
+	pid_t my_pid, child_pid;
+	int n = 0, err, status;
 
 	if (argc > 1)
 	execve(*(argv + 1), (argv + 1), NULL);
 	else
 	{
-
-		
-		size = getline(&av, &len, stdin);
-		*(av + size) = '\0';
-		token = strtok_r(av, " ", &svptr);
-		while (1);
+		while (1)
 		{
-			//str = malloc(sizeof(char) * strlen(token));
-			//strcpy(str, token);
-			printf("gets here\n");
-			*(arg + n) = token;
+			n = 0;
+			printf("%s$ ", SHELL_STR);
+			size = getline(&av, &len, stdin);
+			*(av + size) = '\0';
+			token = strtok_r(av, "' \n'", &svptr);
+			while (token != NULL)
+			{
+				str = malloc(sizeof(char) * strlen(token));
+				strcpy(str, token);
+				*(arg + n) = str;
+				token = strtok_r(NULL, "' \n'", &svptr);
+				n++;
+			}
+			*(arg + n) = NULL;
 
-			token = strtok_r(NULL, " ", &svptr);
-			if (token == NULL)
+			child_pid = fork();
+			if (child_pid == 0)
+			{
+				n = execve(arg[0], arg, NULL);
+				if (n == -1)
+				{
+					err = errno;
+					printf("%s: '%s' No such file or directory\n", argv[0], arg[0]);
+				}
 				break;
-			n++;
-			printf("loop\n");
-		}
+			}
+			else
+				wait(&status);
 
-	}
-	*(arg + n) = NULL;
-	n = 0;
-	while (*(arg + n) != NULL)
-	{
-	       printf("%s\n", *(arg + n));
-	       n++;
+
+			/*n = 0;
+			while (*(arg + n) != NULL)
+			{
+	     			printf("%s\n", *(arg + n));
+				n++;
+			}*/
+		}
 	}
 	return (0);
 }
