ee1ce45e53048c5e65e0eaa82b9c128e3864fddd
Tosam01 <tosam0001@gmail.com>
2022-05-20 15:16:20 +0000

    printers ready

--- printers.c
+++ printers.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
+/**
+ * _putchar - putchar function
+ * @c: value
+ * Return: 1
+ */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
+/**
+ * print_s - print function
+ * @s: value
+ * Return: i
+ */
 
 int print_s(char *s)
 {
